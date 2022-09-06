ddc7933c8b017464095a399f4d38d681a5779e0b
Mark Dembo <mdembo@cloudflare.com>
2022-09-06 16:36:38 +0200

    API-1 Refactor main file

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,71 @@ export interface Env {
   API_KEYS: KVNamespace;
 }
 
+const mainRequestExecution = async (
+  request: Request,
+  env: Env,
+  logger: Logger
+): Promise<Response> => {
+  let statusCode: number = -1;
+  try {
+    const originalUrl = new URL(request.url);
+    logger.debug("Request started", {
+      path: originalUrl.pathname,
+      headers: request.headers,
+    });
+    // Authentication
+    const key = await getKeyFromAuthHeader(request.headers, env.API_KEYS);
+    // Authorization
+    const internalRoute = getRoute(originalUrl.pathname);
+    const response = await fetchRoute(internalRoute, request);
+    statusCode = response.status;
+    return new Response(await response.text(), {
+      status: statusCode,
+      statusText: response.statusText,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      statusCode = 403;
+      return new Response(AUTH_ERROR_MESSAGE, {
+        status: statusCode,
+      });
+    } else if (error instanceof PathError) {
+      statusCode = 404;
+      return new Response(PATH_ERROR_MESSAGE, {
+        status: statusCode,
+      });
+    } else if (error instanceof RequestError) {
+      statusCode = 500;
+      return new Response(REQUEST_ERROR_MESSAGE, {
+        status: statusCode,
+      });
+    } else {
+      throw error;
+    }
+  } finally {
+    logger.debug("Request finished", { statusCode });
+  }
+};
+
+const postRequestExecution = (
+  request: Request,
+  env: Env,
+  ctx: ExecutionContext,
+  logger: Logger
+) => {
+  const originalUrl = new URL(request.url);
+  ctx.waitUntil(
+    Promise.all([
+      logger.flush(),
+      track_api_call(
+        "00_test",
+        originalUrl.pathname,
+        env.MIXPANEL_PROJECT_TOKEN
+      ),
+    ])
+  );
+};
+
 export default {
   async fetch(
     request: Request,
@@ -24,53 +89,8 @@ export default {
   ): Promise<Response> {
     const logProvider = new NewRelicProvider(env.NEW_RELIC_LICENSE_KEY);
     const logger = new Logger(logProvider, { ...request });
-    let statusCode: number = -1;
-    try {
-      const originalUrl = new URL(request.url);
-      logger.debug("Request started", {
-        path: originalUrl.pathname,
-        headers: request.headers,
-      });
-      // Authentication
-      const key = await getKeyFromAuthHeader(request.headers, env.API_KEYS);
-      // Authorization
-      const internalRoute = getRoute(originalUrl.pathname);
-      const response = await fetchRoute(internalRoute, request);
-      statusCode = response.status;
-      ctx.waitUntil(
-        track_api_call(
-          "00_test",
-          originalUrl.pathname,
-          env.MIXPANEL_PROJECT_TOKEN
-        )
-      );
-      return new Response(await response.text(), {
-        status: statusCode,
-        statusText: response.statusText,
-      });
-    } catch (error) {
-      if (error instanceof AuthError) {
-        statusCode = 403;
-        return new Response(AUTH_ERROR_MESSAGE, {
-          status: statusCode,
-        });
-      } else if (error instanceof PathError) {
-        statusCode = 404;
-        return new Response(PATH_ERROR_MESSAGE, {
-          status: statusCode,
-        });
-      } else if (error instanceof RequestError) {
-        statusCode = 500;
-        return new Response(REQUEST_ERROR_MESSAGE, {
-          status: statusCode,
-        });
-      } else {
-        throw error;
-      }
-    } finally {
-      logger.debug("Resquest finished", { statusCode });
-
-      ctx.waitUntil(logger.flush());
-    }
+    const response = mainRequestExecution(request, env, logger);
+    postRequestExecution(request, env, ctx, logger);
+    return response;
   },
 };
